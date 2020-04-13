f08078294ce8054d09abe10c0befd908c063ccdf
Mrucznik <mrucznix@gmail.com>
2020-04-13 20:25:31 +0200

    Auto Creation of Proto v0.1.6

---
+++ characters/CharactersServiceClientPb.ts
@@ -0,0 +1,191 @@
+/**
+ * @fileoverview gRPC-Web generated client stub for mruv
+ * @enhanceable
+ * @public
+ */
+
+// GENERATED CODE -- DO NOT EDIT!
+
+
+import * as grpcWeb from 'grpc-web';
+
+import * as google_api_annotations_pb from '../google/api/annotations_pb';
+import * as common_health_pb from '../common/health_pb';
+import * as characters_characters_model_pb from '../characters/characters_model_pb';
+
+import {
+  DeathStreamRequest,
+  DeathStreamResponse} from './characters_pb';
+
+export class MruVCharactersServiceClient {
+  client_: grpcWeb.AbstractClientBase;
+  hostname_: string;
+  credentials_: null | { [index: string]: string; };
+  options_: null | { [index: string]: string; };
+
+  constructor (hostname: string,
+               credentials?: null | { [index: string]: string; },
+               options?: null | { [index: string]: string; }) {
+    if (!options) options = {};
+    if (!credentials) credentials = {};
+    options['format'] = 'text';
+
+    this.client_ = new grpcWeb.GrpcWebClientBase(options);
+    this.hostname_ = hostname;
+    this.credentials_ = credentials;
+    this.options_ = options;
+  }
+
+  methodInfoCreateCharacter = new grpcWeb.AbstractClientBase.MethodInfo(
+    characters_characters_model_pb.CharacterID,
+    (request: characters_characters_model_pb.Character) => {
+      return request.serializeBinary();
+    },
+    characters_characters_model_pb.CharacterID.deserializeBinary
+  );
+
+  createCharacter(
+    request: characters_characters_model_pb.Character,
+    metadata: grpcWeb.Metadata | null,
+    callback: (err: grpcWeb.Error,
+               response: characters_characters_model_pb.CharacterID) => void) {
+    return this.client_.rpcCall(
+      this.hostname_ +
+        '/mruv.MruVCharactersService/CreateCharacter',
+      request,
+      metadata || {},
+      this.methodInfoCreateCharacter,
+      callback);
+  }
+
+  methodInfoGetCharacter = new grpcWeb.AbstractClientBase.MethodInfo(
+    characters_characters_model_pb.Character,
+    (request: characters_characters_model_pb.CharacterID) => {
+      return request.serializeBinary();
+    },
+    characters_characters_model_pb.Character.deserializeBinary
+  );
+
+  getCharacter(
+    request: characters_characters_model_pb.CharacterID,
+    metadata: grpcWeb.Metadata | null,
+    callback: (err: grpcWeb.Error,
+               response: characters_characters_model_pb.Character) => void) {
+    return this.client_.rpcCall(
+      this.hostname_ +
+        '/mruv.MruVCharactersService/GetCharacter',
+      request,
+      metadata || {},
+      this.methodInfoGetCharacter,
+      callback);
+  }
+
+  methodInfoRemoveCharacter = new grpcWeb.AbstractClientBase.MethodInfo(
+    characters_characters_model_pb.CharacterID,
+    (request: characters_characters_model_pb.CharacterID) => {
+      return request.serializeBinary();
+    },
+    characters_characters_model_pb.CharacterID.deserializeBinary
+  );
+
+  removeCharacter(
+    request: characters_characters_model_pb.CharacterID,
+    metadata: grpcWeb.Metadata | null,
+    callback: (err: grpcWeb.Error,
+               response: characters_characters_model_pb.CharacterID) => void) {
+    return this.client_.rpcCall(
+      this.hostname_ +
+        '/mruv.MruVCharactersService/RemoveCharacter',
+      request,
+      metadata || {},
+      this.methodInfoRemoveCharacter,
+      callback);
+  }
+
+  methodInfoPermanentCharacterKill = new grpcWeb.AbstractClientBase.MethodInfo(
+    characters_characters_model_pb.CharacterID,
+    (request: characters_characters_model_pb.CharacterID) => {
+      return request.serializeBinary();
+    },
+    characters_characters_model_pb.CharacterID.deserializeBinary
+  );
+
+  permanentCharacterKill(
+    request: characters_characters_model_pb.CharacterID,
+    metadata: grpcWeb.Metadata | null,
+    callback: (err: grpcWeb.Error,
+               response: characters_characters_model_pb.CharacterID) => void) {
+    return this.client_.rpcCall(
+      this.hostname_ +
+        '/mruv.MruVCharactersService/PermanentCharacterKill',
+      request,
+      metadata || {},
+      this.methodInfoPermanentCharacterKill,
+      callback);
+  }
+
+  methodInfoDeathsStream = new grpcWeb.AbstractClientBase.MethodInfo(
+    DeathStreamResponse,
+    (request: DeathStreamRequest) => {
+      return request.serializeBinary();
+    },
+    DeathStreamResponse.deserializeBinary
+  );
+
+  deathsStream(
+    request: DeathStreamRequest,
+    metadata?: grpcWeb.Metadata) {
+    return this.client_.serverStreaming(
+      this.hostname_ +
+        '/mruv.MruVCharactersService/DeathsStream',
+      request,
+      metadata || {},
+      this.methodInfoDeathsStream);
+  }
+
+  methodInfoGetServiceStatus = new grpcWeb.AbstractClientBase.MethodInfo(
+    common_health_pb.ServiceStatusResponse,
+    (request: common_health_pb.ServiceStatusRequest) => {
+      return request.serializeBinary();
+    },
+    common_health_pb.ServiceStatusResponse.deserializeBinary
+  );
+
+  getServiceStatus(
+    request: common_health_pb.ServiceStatusRequest,
+    metadata: grpcWeb.Metadata | null,
+    callback: (err: grpcWeb.Error,
+               response: common_health_pb.ServiceStatusResponse) => void) {
+    return this.client_.rpcCall(
+      this.hostname_ +
+        '/mruv.MruVCharactersService/GetServiceStatus',
+      request,
+      metadata || {},
+      this.methodInfoGetServiceStatus,
+      callback);
+  }
+
+  methodInfoGetServiceVersion = new grpcWeb.AbstractClientBase.MethodInfo(
+    common_health_pb.VersionResponse,
+    (request: common_health_pb.VersionRequest) => {
+      return request.serializeBinary();
+    },
+    common_health_pb.VersionResponse.deserializeBinary
+  );
+
+  getServiceVersion(
+    request: common_health_pb.VersionRequest,
+    metadata: grpcWeb.Metadata | null,
+    callback: (err: grpcWeb.Error,
+               response: common_health_pb.VersionResponse) => void) {
+    return this.client_.rpcCall(
+      this.hostname_ +
+        '/mruv.MruVCharactersService/GetServiceVersion',
+      request,
+      metadata || {},
+      this.methodInfoGetServiceVersion,
+      callback);
+  }
+
+}
+
